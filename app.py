27d19d670570cb1df315f0862bcf75ad8c8938f1
vaccarse730 <vaccarse730@gmail.com>
2026-09-19 15:50:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import urllib.request
 import json
 import os
-from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory, jsonify
 from werkzeug.utils import secure_filename
 import psycopg2
 from datetime import datetime
@@ -536,31 +536,6 @@ def actualizar_tipo_cambio():
                 
                 return jsonify({"success": True, "nuevo_valor": nuevo_precio})
     return jsonify({"success": False, "error": "No autorizado o valor inválido"}), 400
-
-@app.route('/actualizar_dolar', methods=['POST'])
-def actualizar_dolar():
-    if 'usuario' in session:
-        puesto_usuario = session['puesto'].lower()
-        if 'administrador' in puesto_usuario or 'administración' in puesto_usuario or 'administracion' in puesto_usuario:
-            nuevo_precio = request.form.get('nuevo_dolar', '').strip()
-            if nuevo_precio:
-                fecha_actual = datetime.now().strftime('%d/%m/%Y')
-                hora_actual = datetime.now().strftime('%H:%M')
-                usuario_cambio = session['usuario']
-                conn = get_db_connection()
-                cursor = conn.cursor()
-                cursor.execute("SELECT valor FROM configuracion WHERE clave = 'dolar'")
-                precio_actual_bd = cursor.fetchone()
-                precio_viejo = precio_actual_bd[0] if precio_actual_bd else "17.35"
-                cursor.execute("UPDATE configuracion SET valor = %s WHERE clave = 'dolar_anterior'", (precio_viejo,))
-                cursor.execute("UPDATE configuracion SET valor = %s WHERE clave = 'dolar'", (nuevo_precio,))
-                cursor.execute("UPDATE configuracion SET valor = %s WHERE clave = 'dolar_fecha'", (fecha_actual,))
-                cursor.execute("UPDATE configuracion SET valor = %s WHERE clave = 'dolar_hora'", (hora_actual,))
-                cursor.execute("UPDATE configuracion SET valor = %s WHERE clave = 'dolar_usuario'", (usuario_cambio,))
-                conn.commit()
-                cursor.close()
-                conn.close()
-    return redirect(url_for('inicio'))
 
 @app.route('/crear_link', methods=['POST'])
 def crear_link():
